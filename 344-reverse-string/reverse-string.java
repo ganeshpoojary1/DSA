bf5034060494656cefd1b24f5f26ca51b738f1fb
ganeshpoojary1 <ganeshpoojari344@gmail.com>
2026-03-27 09:15:03 +0530

    Time: 3 ms (5.81%) | Memory: 47.8 MB (98.86%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+       int start=0;
+       int end=s.length-1;
+
+       while(start<end){
+        char i=s[start];
+        s[start]=s[end];
+        s[end]=i;
+        start++;
+        end--;
+       }
+       System.out.print(s);
+    }
+}
