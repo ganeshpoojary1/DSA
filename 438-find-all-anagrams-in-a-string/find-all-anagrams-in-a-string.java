aa0843c7c3f590aecf9841b819da56b9fab6affe
ganeshpoojary1 <ganeshpoojari344@gmail.com>
2026-05-09 13:19:56 +0530

    Time: 64 ms (20.35%) | Memory: 47.5 MB (21.33%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        ArrayList<Integer> list = new ArrayList<>();
+        HashMap<Character,Integer> map1 = new HashMap<>();
+        HashMap<Character,Integer> map2 = new HashMap<>();
+
+        int left=0;
+        for(char i:p.toCharArray()){
+            if(map1.containsKey(i)){
+                map1.put(i,map1.get(i)+1);
+            }
+            else{
+                map1.put(i,1);
+            }
+        }
+        for(int right=0;right<s.length();right++){
+            char r = s.charAt(right);
+            if(map2.containsKey(r)){
+                map2.put(r,map2.get(r)+1);
+            }
+            else{
+                map2.put(r,1);
+            }
+            char l=s.charAt(left);
+            if((right-left+1)>p.length()){
+                map2.put(l,map2.get(l)-1);
+                left++;
+            }
+            if(map2.get(l)==0){
+                map2.remove(l);
+            }
+            if(map1.equals(map2)){
+              list.add(left);
+            }
+        }
+        return list;
+        
+    }
+}
