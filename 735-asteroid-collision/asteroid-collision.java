cc59bb879d67f673b0b5a5dc57cf4a88cc86feab
ganeshpoojary1 <ganeshpoojari344@gmail.com>
2026-06-11 18:05:33 +0530

    Time: 4 ms (82.56%) | Memory: 47.2 MB (19.21%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,35 +1,40 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack<>();
-       for(int asteroid:asteroids){
-        boolean destroyed = false;
-        while(!stack.isEmpty()&&stack.peek()>0 && asteroid<0){
-            if(stack.peek()< -asteroid){
-                stack.pop();
-            }
-               else if (stack.peek() == -asteroid) {
-                    stack.pop();
-                    destroyed = true;
-                    break;
-                }
-                else {
-                    destroyed = true;
-                    break;
-                }
-            }
-
-            if (!destroyed) {
-                stack.push(asteroid);
-            }
-        }
-
-        int[] result = new int[stack.size()];
-        for (int i = stack.size() - 1; i >= 0; i--) {
-            result[i] = stack.pop();
-        }
-
-        return result;               
         
-       
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<asteroids.length;i++){
+            if(stack.isEmpty() || asteroids[i]>0){
+                stack.push(asteroids[i]);
+            }
+            else{
+                while(!stack.isEmpty()){
+                    int top = stack.peek();
+                    if(top<0){
+                        stack.push(asteroids[i]);
+                        break;
+                    }
+                    int val = Math.abs(asteroids[i]);
+                    if(val==top){
+                        stack.pop();
+                        break;
+                    }
+                    else if(val<top){
+                        break;
+                    }
+                    else{
+                        stack.pop();
+                        if(stack.isEmpty()){
+                            stack.push(asteroids[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int[] result = new int[stack.size()];
+        for(int i=stack.size()-1;i>=0;i--){
+            result[i]=stack.pop();
+        }
+        return result;
     }
 }
