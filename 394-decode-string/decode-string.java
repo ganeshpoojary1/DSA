19906a22ab3b7863434d15289f575b423755948e
ganeshpoojary1 <ganeshpoojari344@gmail.com>
2026-06-16 18:14:44 +0530

    Time: 3 ms (25.89%) | Memory: 43.2 MB (8.79%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,34 +1,33 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<String> StringStack = new Stack<>();
+        Stack<String> stringStack = new Stack<>();
         Stack<Integer> numStack = new Stack<>();
-         int curnum=0;
-         String curString="";
-         for(char c : s.toCharArray()){
+        int curnum=0;
+        String curString = "";
+        for(char c:s.toCharArray()){
             if(c=='['){
-                StringStack.push(curString);
+                stringStack.push(curString);
                 numStack.push(curnum);
-                curnum=0;
                 curString="";
+                curnum=0;
             }
             else if(c==']'){
                 int num=numStack.pop();
-                String prevString=StringStack.pop();
-
+                String prevString=stringStack.pop();
                 StringBuilder temp = new StringBuilder(prevString);
-
                 for(int i=0;i<num;i++){
                     temp.append(curString);
                 }
                 curString=temp.toString();
-           }
-           else if(Character.isDigit(c)){
-            curnum=curnum*10+(c - '0');
-           }
-           else{
-            curString+=c;
-           }
-         }
-         return curString; 
+            }
+            else if(Character.isDigit(c)){
+                curnum = curnum*10 + c - '0';
+            }
+            else{
+                curString+=c;
+            }
+        }
+        return curString;
+        
     }
 }
