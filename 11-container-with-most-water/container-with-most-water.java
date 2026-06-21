aa5802adff61668b48eff4924c28c0061e52fc14
ganeshpoojary1 <ganeshpoojari344@gmail.com>
2026-06-21 23:55:59 +0530

    Time: 5 ms (82.69%) | Memory: 77.5 MB (28.33%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,14 +1,11 @@
 class Solution {
     public int maxArea(int[] height) {
-        
         int left=0;
         int right=height.length-1;
         int max=0;
-
         while(left<right){
             int h=Math.min(height[left],height[right]);
             max=Math.max(max,h*(right-left));
-
             if(height[left]<height[right]){
                 left++;
             }
@@ -17,5 +14,6 @@ class Solution {
             }
         }
         return max;
+        
     }
 }
