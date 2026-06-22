c94a47f98b137a688bed444080032d53aca17604
ganeshpoojary1 <ganeshpoojari344@gmail.com>
2026-06-23 00:32:32 +0530

    Time: 236 ms (5.04%) | Memory: 47.9 MB (5.75%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,24 +1,40 @@
 class Solution {
     public String reverseVowels(String s) {
-        char[] ch=s.toCharArray();
-        String vowels="AEIOUaeiou";
-
-        int start=0;
-        int end=ch.length-1;
-
-        while(start<end){
-            while(start < end && vowels.indexOf(ch[start])==-1 ){
-                start++;
+        char[] a = s.toCharArray();
+        int i=0;
+        int j=a.length-1;
+        while(i<j){
+        while(i<=a.length-1){
+            if(a[i]=='A' || a[i]=='E' || a[i]=='I' || a[i]=='O' || a[i]=='U'){
+                break;
             }
-            while(start<end && vowels.indexOf(ch[end])==-1){
-                end--;
+            else if(a[i]=='a' || a[i]=='e' || a[i]=='i' || a[i]=='o' || a[i]=='u'){
+                break;
             }
-            char temp = ch[start];
-            ch[start]=ch[end];
-            ch[end]=temp;
-            start++;
-            end--;
+            i++;
         }
-        return new String(ch);
+        while(j>=0){
+             if(a[j]=='A' || a[j]=='E' || a[j]=='I' || a[j]=='O' || a[j]=='U'){
+                break;
+            }
+             else if(a[j]=='a' || a[j]=='e' || a[j]=='i' || a[j]=='o' || a[j]=='u'){
+                break;
+            }
+            j--;
+        }
+        
+        if(i<j){
+            char x = a[i];
+            a[i]=a[j];
+            a[j]=x;
+            i++;
+            j--;
+        }
+        }
+        s="";
+        for(char k:a){
+            s=s+k;
+        }
+        return s;
     }
 }
