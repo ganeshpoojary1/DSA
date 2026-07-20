4a85cbe8a54153ae20695b5e6594cbd20bd56d00
ganeshpoojary1 <ganeshpoojari344@gmail.com>
2026-07-20 21:26:15 +0530

    Time: 1 ms (98.40%) | Memory: 47.1 MB (93.94%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -6,10 +6,13 @@ class Solution {
         }
         int leftsum=0;
         for(int i=0;i<nums.length;i++){
-            if(leftsum==sum-leftsum-nums[i])
-            return i;
+            if(leftsum==(sum-leftsum-nums[i])){
+                return i;
+            }
             leftsum+=nums[i];
         }
         return -1;
+
+        
     }
 }
