1c8e6c589bd7056f2a1a84460d6e0454fd1c178a
ganeshpoojary1 <ganeshpoojari344@gmail.com>
2026-07-31 01:10:11 +0530

    Time: 9 ms (94.22%) | Memory: 48 MB (73.70%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -8,21 +8,8 @@ class Solution {
         Arrays.sort(arr);
         int count=0;
         int min=0;
-        for(int i=25;i>=0;i--){
-            int mul=0;
-            if(count<8){
-                mul=1;
-            }
-            else if(count<16){
-                mul=2;
-            }
-            else if(count<24){
-                mul=3;
-            }
-            else{
-                mul=4;
-            }
-            int val=arr[i]*mul;
+        for(int i=25;i>=0;i--){ 
+            int val=arr[i]*(count/8+1);
             min+=val;
             count++;
         }
