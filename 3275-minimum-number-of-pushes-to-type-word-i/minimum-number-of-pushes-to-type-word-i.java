11e38d11db127675e689ae20751f60f8c58907f2
ganeshpoojary1 <ganeshpoojari344@gmail.com>
2026-07-30 23:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (66.51%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -3,18 +3,7 @@ class Solution {
         int n=word.length();
         int sum=0;
         for(int i=0;i<n;i++){
-            if(i<8){
-                sum+=1;
-            }
-            else if(i<16){
-                sum+=2;
-            }
-            else if(i<24){
-                sum+=3;
-            }
-            else{
-                sum+=4;
-            }
+            sum+=i/8+1;
         }
         return sum;
     }
