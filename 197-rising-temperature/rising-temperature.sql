3e085da608d1127e4c681a8e00751485b9d153f1
ganeshpoojary1 <ganeshpoojari344@gmail.com>
2026-08-15 11:22:12 +0530

    Time: 570 ms (25.96%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -2,6 +2,5 @@
 SELECT today.id
 FROM Weather today
 INNER JOIN Weather yesterday
-WHERE DATEDIFF(today.recordDate,yesterday.recordDate)=1
-AND today.temperature>yesterday.temperature;
-
+ON DATEDIFF(today.recordDate,yesterday.recordDate)=1
+WHERE today.temperature>yesterday.temperature;
